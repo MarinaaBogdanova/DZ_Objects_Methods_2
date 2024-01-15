6b8ee04e3fbbfbab9c25221701e4e2bd8474cb2f
Marina <marinasoon@gmail.com>
2024-01-15 19:29:48 +0300

    Initial commit2

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,6 @@
 public class BmiService {
     public int calculate(double height, int weight) {
         int index;
-        index = (int) (weight / height);
-        return index;
+        return index = (int) (weight / (height * height));
     }
 }
